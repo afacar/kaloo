d175ea56a518bd493c08a8527b5048cd09671127
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-04-07 15:38:55 +0300

    Screen Awake linking

--- android/app/src/main/java/com/influenceme/MainApplication.java
+++ android/app/src/main/java/com/influenceme/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.corbt.keepawake.KCKeepAwakePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; // <-- Add this line
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; // <-- Add this line
