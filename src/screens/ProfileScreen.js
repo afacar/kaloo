978999f75d399f3ffdb19e2cf2696156ea5f22b0
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-04-19 16:27:24 +0300

    Profile and balance screens customized

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -5,27 +5,23 @@ import { firestore, auth, storage } from "react-native-firebase";
 import ImagePicker from "react-native-image-crop-picker";
 import { ClickableText } from '../components/Buttons';
 import { ContactUs } from '../components/ContactUs';
+import HeaderLeft from '../components/Headers/HeaderLeft';
+import { colors } from '../constants';
+import { AppText } from '../components/Labels';
 
 const db = firestore();
 
 class ProfileScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
+        headerStyle: { backgroundColor: colors.BLUE, borderBottomWidth: 0, elevation: 0, shadowOpacity: 0 },
+        headerTitle: () => null,
+        headerLeft: () => (
+            <HeaderLeft onPress={navigation.goBack} />
+        ),
         headerRight: () => (
-            <Button
-                buttonStyle={{ marginRight: 10 }}
-                title='Wallet'
-                titleStyle={{ color: 'grey' }}
-                type='clear'
-                iconRight
-                icon={<Icon
-                    type='material-community'
-                    name='wallet-outline'
-                    color='grey'
-                    containerStyle={{ marginLeft: 5 }}
-                />}
-                onPress={() => navigation.navigate('Balance')}
-            />
-
+            <View style={{ marginRight: 10 }}>
+                <ClickableText color='white' text='Logout' onPress={() => auth().signOut()} />
+            </View>
         )
     });
 
@@ -36,7 +32,7 @@ class ProfileScreen extends Component {
         photoURL: this.user.photoURL,
         isNameChanged: false,
         isAvatarChanged: false,
-        isWaiting: false,
+        saveLoading: false,
     }
 
     componentDidMount() { }
@@ -45,7 +41,7 @@ class ProfileScreen extends Component {
         let { displayName, photoURL, isAvatarChanged, isNameChanged } = this.state;
         let newProfile = {}
         let isResizedImage = true;
-        this.setState({ isWaiting: true })
+        this.setState({ saveLoading: true })
         if (isAvatarChanged) {
             // Upload new Avatar to @Storage
             newProfile.photoURL = photoURL
@@ -71,7 +67,7 @@ class ProfileScreen extends Component {
         let userRef = db.doc(`users/${this.user.uid}`)
         await userRef.set({ ...newProfile, isResizedImage }, { merge: true });
         console.log('user @db updated ', newProfile);
-        this.setState({ isNameChanged: false, isAvatarChanged: false, isWaiting: false })
+        this.setState({ isNameChanged: false, isAvatarChanged: false, saveLoading: false })
     }
 
     onImagePicker = () => {
@@ -90,44 +86,61 @@ class ProfileScreen extends Component {
     }
 
     render() {
-        const { email, displayName, photoURL, isAvatarChanged, isNameChanged, isWaiting } = this.state
+        const { email, displayName, photoURL, isAvatarChanged, isNameChanged, saveLoading } = this.state
         return (
             <KeyboardAvoidingView style={styles.container}>
-                <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between', paddingHorizontal: 40, paddingVertical: 10, alignItems: 'center' }} >
-                    <View style={{ alignSelf: 'stretch', alignItems: 'center' }}>
+                <ScrollView contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 40, paddingVertical: 10, borderTopLeftRadius: 16, borderTopRightRadius: 16, backgroundColor: 'white' }} >
+                    <View style={{ alignItems: 'flex-start', marginTop: 20 }}>
+                        <AppText style={{ fontSize: 28, fontWeight: 'bold' }}> Edit Profile</AppText>
+                    </View>
+                    <View style={{ marginTop: 20, flexDirection: 'row', width: '100%', }}>
                         <Avatar
-                            //onPress={this.onAvatarPressed}
                             renderPlaceholderContent={<ActivityIndicator />}
-                            onEditPress={this.onImagePicker}
+                            containerStyle={{ alignSelf: 'flex-start' }}
                             size='xlarge'
                             rounded={true}
-                            showEditButton={true}
                             source={{ uri: photoURL }}
                         />
+                        <View style={{ alignSelf: 'center', justifyContent: 'center', marginLeft: 60, }}>
+                            <TouchableOpacity onPress={this.onImagePicker} >
+                                <Text style={{ fontSize: 18, color: colors.BLUE }}>Change Profile Picture</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    <View style={{ marginTop: 20 }}>
+                        <AppText>Name</AppText>
+                        <Input
+                            placeholder='Display name'
+                            onChangeText={displayName => this.setState({ displayName, isNameChanged: true })}
+                            value={displayName}
+                            containerStyle={{ borderWidth: 1, borderRadius: 6, marginTop: 8 }}
+                            inputContainerStyle={{ borderBottomWidth: 0 }}
+                        />
+                    </View>
+
+                    <View style={{ marginTop: 20 }}>
+                        <AppText>E-mail</AppText>
                         <Input
                             placeholder='Enter Email'
                             leftIcon={{ type: 'material-community', name: 'email' }}
-                            //onChangeText={email => this.setState({ email })}
                             value={email}
                             disabled
+                            containerStyle={{ borderWidth: 1, borderRadius: 6, marginTop: 8 }}
+                            inputContainerStyle={{ borderBottomWidth: 0 }}
                         />
-                        <Input
-                            placeholder='Display name'
-                            leftIcon={{ type: 'material-community', name: 'account-circle' }}
-                            onChangeText={displayName => this.setState({ displayName, isNameChanged: true })}
-                            value={displayName}
-                        />
-                        <View style={{ alignSelf: 'stretch', marginTop: 15 }}>
-                            <Button
-                                buttonStyle={{ backgroundColor: 'grey' }}
-                                title={isWaiting ? "Updating..." : 'Update'}
-                                onPress={this.handleProfileUpdate}
-                                disabled={!isNameChanged && !isAvatarChanged || isWaiting}
-                            />
-                        </View>
                     </View>
-                    <View style={{ alignSelf: 'stretch', justifyContent: 'space-around', flexDirection: 'row' }}>
-                        <ClickableText text='Log out' onPress={() => auth().signOut()} />
+
+                    <Button
+                        title={isNameChanged ? 'Save Changes' : 'Edit Your Profile'}
+                        buttonStyle={styles.saveButton}
+                        containerStyle={styles.saveButtonContainer}
+                        onPress={this.handleProfileUpdate}
+                        loading={saveLoading}
+                        disabled={!(isNameChanged || isAvatarChanged)}
+                    />
+
+                    <View style={{ position: 'absolute', bottom: 24, alignSelf: 'center' }}>
                         <ContactUs title='Need Help?' screen='Profile' />
                     </View>
                 </ScrollView>
@@ -139,6 +152,21 @@ class ProfileScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: colors.BLUE
+    },
+    saveButtonContainer: {
+        position: 'absolute',
+        alignSelf: 'center',
+        bottom: 72,
+    },
+    saveButton: {
+        backgroundColor: colors.CYAN,
+        width: 300,
+        height: 50,
+        borderRadius: 16,
+        padding: 12,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
 
