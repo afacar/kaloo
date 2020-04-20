ee611961ef9b40ab0d25633a01b265e1ad4f0ef0
afacar <acarahmetfaruk@gmail.com>
2020-04-20 23:24:23 +0300

    firebase.initializeApp(firebaseConfig) removed

--- App.js
+++ App.js
@@ -9,20 +9,7 @@ import store from './src/appstate/store';
 
 class App extends Component {
   componentDidMount() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyDjzyO4Op-liCcDo_0qj5bJb60vHL0g864",
-      authDomain: "influenceme-dev.firebaseapp.com",
-      databaseURL: "https://influenceme-dev.firebaseio.com",
-      projectId: "influenceme-dev",
-      storageBucket: "influenceme-dev.appspot.com",
-      messagingSenderId: "248240010646",
-      appId: "1:248240010646:web:8c57c3b81404d402b519b7",
-      measurementId: "G-R7D5SZVYFM"
-    };
-    // Initialize Firebase
-    if (!firebase.apps.length)
-      firebase.initializeApp(firebaseConfig);
-
+    console.log('firebase apps', firebase.apps)
   }
 
   render() {
