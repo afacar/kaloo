548cf4875cba3b91ad3ec6d4224ae3a0466f1a23
afacar <acarahmetfaruk@gmail.com>
2020-04-24 03:06:04 +0300

    MyTicketScreen cleaned

--- src/screens/MyTicketScreen.js
+++ src/screens/MyTicketScreen.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import Ticket from '../components/Ticket';
-import { colors } from '../constants';
 import HeaderLeft from '../components/Headers/HeaderLeft';
 
 
 class MyTicketScreen extends Component {
-    static navigationOptions = ({ navigation }) => ({
-        headerStyle: { backgroundColor: colors.BLUE },
-        headerTitle: () => null,
-        headerLeft: () => <HeaderLeft onPress={navigation.goBack} />
-    });
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: () => null,
+    headerLeft: () => <HeaderLeft onPress={navigation.goBack} />
+  });
 
-    render() {
-        return <Ticket navigation={this.props.navigation} />
-    }
+  render() {
+    return <Ticket navigation={this.props.navigation} />
+  }
 }
 
 export default MyTicketScreen;
