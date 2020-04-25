5d0ff2e7a953670dbb50923c03c94b57db120a64
afacar <acarahmetfaruk@gmail.com>
2020-04-25 23:42:01 +0300

    JoinEvent component renamed as GuestView

--- src/components/GuestView.js
+++ src/components/GuestView.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, NativeModules, ScrollView } from 'react-native';
-import { Button, Card, AirbnbRating } from 'react-native-elements';
+import { Card } from 'react-native-elements';
 import { RtcEngine } from 'react-native-agora';
-import { connect } from 'react-redux';
+import { auth } from 'react-native-firebase';
 
-import * as actions from '../appstate/actions/audience_actions';
 import { app } from '../constants';
 import { AppText } from "./Labels";
 import { joinEvent, rateEvent } from "../utils/EventHandler";
@@ -12,9 +11,9 @@ import PreviewHeader from './PreviewHeader';
 import PreviewBody from './PreviewBody';
 import CustomStatusBar from './StatusBars/CustomStatusBar';
 import { DefaultButton } from './Buttons';
-import { auth } from 'react-native-firebase';
+import RatingView from './RatingView';
 
-const { MEETING, BROADCAST } = app.EVENT_TYPE;
+const { CALL, BROADCAST } = app.EVENT_TYPE;
 
 const { Agora } = NativeModules;
 const { SCHEDULED, SUSPENDED, IN_PROGRESS, COMPLETED } = app.EVENT_STATUS
@@ -26,7 +25,7 @@ const {
     Adaptative
 } = Agora
 
-class JoinEvent extends Component {
+class GuestView extends Component {
     state = {
         isRatingComplete: false,
         joinLoading: false,
@@ -34,26 +33,21 @@ class JoinEvent extends Component {
         error: undefined
     }
 
-    componentDidMount() {
-        console.log('Event state is', this.state)
-    }
+    componentDidMount() { }
 
-    componentWillUnmount() {
-        //clearEventListener(this.event.eid);
-        this.props.clearJoinEventListener()
-    }
+    componentWillUnmount() { }
 
     onCamera = async () => {
         this.setState({
             joinLoading: true,
             error: undefined
         })
-        const { eventType, eventId, duration } = this.props.event
+        const { eventType, eventId } = this.props.event
         const ticket = this.props.ticket
         const { navigate } = this.props.navigation
-        // live channelProfile: 1 & call channelProfile: 0
+        // Broadcast channelProfile: 1 & Call channelProfile: 0
         let channelProfile = eventType === BROADCAST ? 1 : 0
-        // Audience clientRole: 2
+        // Guest clientRole: 2
         let clientRole = 2
         const options = {
             appid: app.AGORA_APP_ID,
@@ -76,9 +70,9 @@ class JoinEvent extends Component {
             console.log('audience joinning channel with', eventId, ticket.index)
             RtcEngine.joinChannel(eventId, parseInt(ticket.index))
                 .then((res) => {
-                    let my = auth().currentUser ? 'My' : ''
-                    let nextScreen = eventType === BROADCAST ? 'Broadcast' : 'Meeting'
-                    navigate(my + nextScreen)
+                    let nextScreen = auth().currentUser ? '' : 'A'
+                    nextScreen += eventType === BROADCAST ? 'Broadcast' : 'Call'
+                    navigate(nextScreen)
                 })
                 .catch((error) => {
                     this.setState({ error: error.message || 'Check your connection' })
@@ -100,11 +94,12 @@ class JoinEvent extends Component {
     }
 
     render() {
-        const eventData = this.props.event || this.props.navigation.getParam('event')
-        const ticketData = this.props.ticket || this.props.navigation.getParam('event').ticket
+        const eventData = this.props.event
+        const ticketData = this.props.ticket
+        const viewers = this.props.viewers // TODO: Maybe number of online viewes can be shown QuestView later
         const { image, photoURL, title, description, displayName, duration, eventType, eventDate, status, joinLoading } = eventData;
         const disabled = status !== IN_PROGRESS
-        const eventTypeName = eventType === MEETING ? 'Meeting' : 'Broadcast'
+        const eventTypeName = eventType === CALL ? 'Meeting' : 'Broadcast'
         const buttonTitle = (status === COMPLETED) ? `${eventTypeName} Finished` : (status === SCHEDULED || status === SUSPENDED) ? 'Waiting Host' : `Join ${eventTypeName}`;
         return (
             <ScrollView contentContainerStyle={styles.container}>
@@ -123,31 +118,7 @@ class JoinEvent extends Component {
                             disabled={disabled}
                             loading={joinLoading}
                         />
-                        <View>
-                            {
-                                (status === COMPLETED && !this.state.isRatingComplete && !ticketData.rate) && (
-                                    <View>
-                                        <AirbnbRating
-                                            count={5}
-                                            reviews={["Terrible", "Bad", "OK", "Good", "Unbelievable"]}
-                                            defaultRating={3}
-                                            onFinishRating={this.changeRate}
-                                            size={20}
-                                        />
-                                        <Button
-                                            title="Submit"
-                                            buttonStyle={{ marginTop: 12, marginBottom: 12, width: 180, height: 40, alignSelf: 'center', borderRadius: 6 }}
-                                            onPress={this.rateEvent}
-                                        />
-                                    </View>
-                                )
-                            }
-                            {
-                                (this.state.isRatingComplete || ticketData.rate) && (
-                                    <AppText style={{ fontSize: 16, alignSelf: 'center', marginTop: 12 }}>Thanks For Your Feedback</AppText>
-                                )
-                            }
-                        </View>
+                        <RatingView event={eventData} ticket={ticketData} />
                         {
                             this.state.error && (
                                 <AppText style={{ alignSelf: 'center', fontSize: 16, marginTop: 8, textAlign: 'left', color: 'red' }}>
@@ -170,10 +141,4 @@ const styles = StyleSheet.create({
     }
 })
 
-const mapStateToProps = ({ joinEvent }) => {
-    console.log('joinEvent mapStateToProps', joinEvent)
-    const { event, ticket } = joinEvent
-    return { event, ticket }
-}
-
-export default connect(mapStateToProps, actions)(JoinEvent);
+export default GuestView;
