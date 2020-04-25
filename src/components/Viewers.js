07a036975fcf900a5b2311d12bb82f278d3aaff4
afacar <acarahmetfaruk@gmail.com>
2020-04-25 23:43:54 +0300

    Online audience moved as Viewers component

---
+++ src/components/Viewers.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { dimensions, colors } from '../constants';
+import { AppText } from './Labels';
+
+
+export default function Viewers(props) {
+  const { viewers } = props
+
+  return (
+    <View style={styles.container}>
+      <AppText style={styles.viewer}>{viewers}</AppText>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 24 + dimensions.HEADER_MARGIN,
+    left: 24,
+    backgroundColor: 'transparent'
+  },
+  viewer: {
+    fontSize: 12,
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 6,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 8,
+  },
+})
