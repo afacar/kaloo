a20e50ffd2fa3c824137a440e5d75626de81abb2
afacar <acarahmetfaruk@gmail.com>
2020-04-26 16:53:02 +0300

    Waiting renamed Connecting component

--- src/components/CallView.js
+++ src/components/CallView.js
@@ -8,7 +8,7 @@ import Timer from './Timer';
 
 const { IN_PROGRESS } = app.EVENT_STATUS
 
-function WaitingComponent(props) {
+function Connecting(props) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', borderWidth: 2, borderColor: 'purple' }}>
       <Image
@@ -26,19 +26,18 @@ export default function CallView(props) {
   const { peerIds } = props
   const { status } = props.event
   const capacity = peerIds.length;
-  console.log('peerIds at CallView', props)
   if (status === IN_PROGRESS) {
     return (
       <View style={{ flex: 1, borderWidth: 2, borderColor: 'yellow', zIndex: -1100 }}>
-        {/** Viewer */}
+        {/** Remote Stream */}
         <View style={{ flex: 1 }}>
-          {capacity == 0 ? (<WaitingComponent />) :
+          {capacity == 0 ? (<Connecting />) :
             <View style={{ flex: 1 }}>
               <AgoraView mode={1} key={peerIds[0]} style={{ flex: 1 }} remoteUid={peerIds[0]} />
             </View>
           }
         </View>
-        {/** Mirror */}
+        {/** Local Stream */}
         <View style={{ flex: 1, borderWidth: 5, borderColor: 'orange' }}>
           <AgoraView style={{ flex: 1 }} showLocalVideo={true} mode={1} />
         </View>
@@ -48,7 +47,7 @@ export default function CallView(props) {
   } else {
     return (
       <View style={{ flex: 1, borderWidth: 2, borderColor: 'yellow', zIndex: -1100 }}>
-        {/** Mirror */}
+        {/** Local Stream */}
         <AgoraView style={{ flex: 1 }} showLocalVideo={true} mode={1} />
         <Timer event={props.event} />
       </View>
