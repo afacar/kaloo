301d7c81824e0f6daecb9f52f9e803bfaa9989dd
afacar <acarahmetfaruk@gmail.com>
2020-04-26 16:53:35 +0300

    Conditional SwitchCamera button rendering

--- src/components/Headers/SwitchCamera.js
+++ src/components/Headers/SwitchCamera.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { RtcEngine } from 'react-native-agora';
+import { connect } from 'react-redux';
+
+import app from '../../constants/app';
+const { CALL } = app.EVENT_TYPE
 
 
-export default function SwitchCamera(props) {
-    return (
-        <TouchableOpacity
-            style={{ flex: 1, marginRight: 10, justifyContent: 'center' }}
-            onPress={() => RtcEngine.switchCamera()}>
-            <Icon
-                name='camera-switch'
-                type='material-community'
-                color='white'
-            />
-        </TouchableOpacity>
-    )
+class SwitchCamera extends React.Component {
+
+    render() {
+        const { clientRole, eventType } = this.props
+        if (clientRole === 1 || eventType === CALL) {
+            return (
+                <TouchableOpacity
+                    style={{ flex: 1, marginRight: 10, justifyContent: 'center' }}
+                    onPress={() => RtcEngine.switchCamera()}>
+                    <Icon
+                        name='camera-switch'
+                        type='material-community'
+                        color='white'
+                    />
+                </TouchableOpacity>
+            )
+        } else {
+            return <View></View>
+        }
+    }
 }
+
+const mapStateToProps = ({ joinEvent }) => {
+    const { event } = joinEvent
+    console.log('SwitchCamera mapStateToProps status', joinEvent)
+    return { eventType: event.eventType }
+}
+
+export default connect(mapStateToProps, null)(SwitchCamera)
