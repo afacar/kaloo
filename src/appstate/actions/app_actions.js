2695d2fff24803f42dbf9b55411d6864bac454c0
afacar <acarahmetfaruk@gmail.com>
2020-04-30 18:23:24 +0300

    app assets renamed

--- src/appstate/actions/app_actions.js
+++ src/appstate/actions/app_actions.js
@@ -1,6 +1,5 @@
-import { firestore, storage } from "react-native-firebase";
+import { firestore } from "react-native-firebase";
 import { LOAD_ASSETS } from "./types"
-import { app } from "../../constants";
 
 const db = firestore();
 
@@ -8,16 +7,10 @@ export const loadAssets = () => async (dispatch) => {
     // Fetch static assets from Firestore
     let assetsDoc = await db.doc(`assets/static`).get();
 
-    let staticAssets = assetsDoc.data()
-    let TICKET_FORMAT = staticAssets.TICKET_PLACEHOLDER
-    let DEFAULT_LOGO_IMAGE = staticAssets.DEFAULT_LOGO
-    let DEFAULT_EVENT_IMAGE = staticAssets.DEFAULT_EVENT
-    let DEFAULT_PROFILE_IMAGE = staticAssets.DEFAULT_PROFILE
-    let HOST_CONNECTING_IMAGE = staticAssets.HOST_CONNECTING
-    let NO_EVENT_IMAGE = staticAssets.NO_EVENT
+    let assets = assetsDoc.data()
 
     return dispatch({
         type: LOAD_ASSETS,
-        payload: { TICKET_FORMAT, DEFAULT_EVENT_IMAGE, DEFAULT_LOGO_IMAGE, DEFAULT_PROFILE_IMAGE, HOST_CONNECTING_IMAGE, NO_EVENT_IMAGE }
+        payload: assets
     })
 }
--- src/appstate/reducers/app_reducers.js
+++ src/appstate/reducers/app_reducers.js
@@ -1,18 +1,16 @@
 import { LOAD_ASSETS } from "../actions/types";
 
 const INITIAL_STATE = {
-    assets: {
-        TICKET_FORMAT: '',
-        DEFAULT_EVENT_IMAGE: '',
-        DEFAULT_LOGO_IMAGE: '',
-        DEFAULT_PROFILE_IMAGE: '',
-    }
+    TICKET_FORMAT: '',
+    DEFAULT_EVENT_IMAGE: '',
+    DEFAULT_LOGO_IMAGE: '',
+    DEFAULT_PROFILE_IMAGE: '',
 }
 
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
         case LOAD_ASSETS: {
-            return { ...state, assets: action.payload }
+            return { ...state, ...action.payload }
         }
         default:
             return state;
